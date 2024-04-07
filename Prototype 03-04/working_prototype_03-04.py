c7e3cde812bc21aeffad0a452edfe7a2bb7f7b9a
Alberto Lorente Galé <145107598+alberto-lorente@users.noreply.github.com>
2024-04-07 19:24:40 +0200

    Delete Prototype 03-04 directory

--- Prototype 03-04/working_prototype_03-04.py
+++
@@ -1,8 +0,0 @@
-from Functions.RorschIA_functions_03_04 import raw_text_response_eval as eval
-
-file_path = input("Copy and paste the path of the text doc.\n\n")
-
-with open(file_path, "r") as f:
-    text = f.read()
-
-evaluation = eval(text)
